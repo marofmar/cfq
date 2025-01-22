228c66106af938e0ebb2e3f3e1ba3341929aa7f3
Yujin <yujin.mar.chung@gmail.com>
2025-01-22 10:31:09 +0900

    add: added new files for the project

---
+++ lib/presentation/pages/wod_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WodPage extends StatelessWidget {
+  const WodPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Wod Page')),
+    );
+  }
+}
